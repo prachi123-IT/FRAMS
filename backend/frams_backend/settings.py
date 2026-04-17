43d7496b826cf9a72164b1db9fd5ab4c1306fa34
prachi123-IT <prachijoshi3052004@gmail.com>
2026-04-17 23:13:10 +0530

    production settings

--- backend/frams_backend/settings.py
+++ backend/frams_backend/settings.py
@@ -21,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xszho7-3e3i-eay)iz@0#9c!@w36s%wf%an#+0b0r^(qs^jbnf'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = os.environ.get("DEBUG") == "True"
 
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 # Application definition
 
