8e480ea0b3e05936c2a5f9bc4ce9ebca569e71de
prachi123-IT <prachijoshi3052004@gmail.com>
2026-04-17 23:25:30 +0530

    add gunicorn

--- backend/frams_backend/settings.py
+++ backend/frams_backend/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 AUTH_USER_MODEL = 'accounts.User'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
